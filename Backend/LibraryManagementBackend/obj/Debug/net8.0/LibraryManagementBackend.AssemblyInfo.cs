0b9fef2f8c30cbe8d8ca67c278700a86d9ddbed0
Dhanawardhana G.A.C.G. <109666856+ChalanaGayan@users.noreply.github.com>
2024-10-13 18:13:53 +0530

    change Branch

--- Backend/LibraryManagementBackend/obj/Debug/net8.0/LibraryManagementBackend.AssemblyInfo.cs
+++ Backend/LibraryManagementBackend/obj/Debug/net8.0/LibraryManagementBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab55dabc194245a2d7f04dc2b385d607711f289")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
