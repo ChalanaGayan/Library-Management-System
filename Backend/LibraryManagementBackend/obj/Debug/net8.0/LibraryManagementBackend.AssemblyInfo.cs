60ebe13980917a9a3aa36bf9caa69567cf032a13
Dhanawardhana G.A.C.G. <109666856+ChalanaGayan@users.noreply.github.com>
2024-10-15 21:04:10 +0530

    s

--- Backend/LibraryManagementBackend/obj/Debug/net8.0/LibraryManagementBackend.AssemblyInfo.cs
+++ Backend/LibraryManagementBackend/obj/Debug/net8.0/LibraryManagementBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+616d1aca4c7af6776a15977ee65e0353105bb58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6540dd6dd314cd17038c50c2dd5ae519403e030")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
